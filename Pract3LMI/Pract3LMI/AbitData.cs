2d5e4855e07582e328c5bb32b84d000d64bf538e
Dmytro-ship <72102026+Dmytro-ship@users.noreply.github.com>
2020-11-18 00:56:57 +0200

    Add files via upload

---
+++ Pract3LMI/Pract3LMI/AbitData.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace MVVM
+{
+    public class AbitData : INotifyPropertyChanged
+    {
+        private Abiturient selectedAbiturient;
+
+        public ObservableCollection<Abiturient> Abiturients { get; set; }
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Abiturient abiturient = new Abiturient();
+                      Abiturients.Insert(0, abiturient);
+                      SelectedAbiturient = abiturient;
+                  }));
+            }
+        }
+        public Abiturient SelectedAbiturient
+        {
+            get { return selectedAbiturient; }
+            set
+            {
+                selectedAbiturient = value;
+                OnPropertyChanged("SelectedAbiturient");
+            }
+        }
+
+        public AbitData()
+        {
+            Abiturients = new ObservableCollection<Abiturient>
+            {
+                new Abiturient {Name="Олег", Surname="Бахматюк", Databirth="10.02.2002", ZNO=140, Bal=6.7, School="Школа номер 3" },
+                new Abiturient {Name="Оксана", Surname="Семенюк", Databirth ="03.07.2001", ZNO=156, Bal=8, School="Педагогічний коледж" },
+                new Abiturient {Name="В'ячеслав", Surname="Знамеровський", Databirth="22.11.2001", ZNO=128, Bal=5.2, School="14 Училище" },
+                new Abiturient {Name="Аліна", Surname="Крайдуба", Databirth="30.05.2002", ZNO=162, Bal=10.1, School="Гімназія" }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Pract3LMI/Pract3LMI/Abiturient.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM
+{
+    public class Abiturient : INotifyPropertyChanged
+    {
+        private string name;
+        private string surname;
+        private string datebirth;
+        private int zno;
+        private double bal;
+        private string school;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                surname = value;
+                OnPropertyChanged("Surname");
+            }
+        }
+        public string Databirth
+        {
+            get { return datebirth; }
+            set
+            {
+               datebirth = value;
+                OnPropertyChanged("Darabirth");
+            }
+        }
+        public int ZNO
+        {
+            get { return zno; }
+            set
+            {
+                zno = value;
+                OnPropertyChanged("ZNO");
+            }
+        }
+        public double Bal
+        {
+            get { return bal; }
+            set
+            {
+                bal = value;
+                OnPropertyChanged("Bal");
+            }
+        }
+        public string School
+        {
+            get { return school; }
+            set
+            {
+                school = value;
+                OnPropertyChanged("School");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
